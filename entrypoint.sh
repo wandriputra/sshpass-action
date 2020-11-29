e8ebe026b19891dd0c4dea19bc509c2ae438322b
Wandri Putra <wandriputra@gmail.com>
2020-11-29 18:28:46 +0700

    Update entrypoint.sh
    port into action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,13 @@ echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 HOST=$1
-USERNAME=$2     
-RUN=$4
+USERNAME=$2
+PORT=$4
+RUN=$5
 export SSHPASS=$3
 
 CMD="${RUN/$'\n'/' && '}"
-sshpass -e ssh -o StrictHostKeyChecking=no $USERNAME@$HOST "$CMD"
+sshpass -e ssh -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST: "$CMD"
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
